a4f6b1aeb833046e9bea499e0729a45967eeac04
Bo-Han Huang <120561501+SamHuang1018@users.noreply.github.com>
2024-07-30 17:11:33 +0800

    Create config.py

---
+++ utils/config.py
@@ -0,0 +1,30 @@
+#####API_KEY#####
+openai_api_key = ''
+gemini_api_key = ''
+channel_access_token = ''
+channel_secret = ''
+search_engine_id = ''
+
+
+
+#####Config#####
+google_search_api = ''
+# ffmpeg = ''
+# fetch_data_url = ''
+model_name = "gpt-4o-2024-05-13"
+temperature = 0.5
+top_p = 0.6
+firebase_url = ''
+home_dir = ''
+markmap_download_dir = ''
+markmap_html_url = ''
+
+#####Fast-Whisper Config#####
+# model_size (str): tiny、base、small、medium、large、large-v2、large-v3
+# device (str, 可選): CPU、GPU(cuda)、Auto
+# compute_type (str, 可選): float16、float32、int8
+# prompt (str, 可選): 依據使用者想要的提示
+model_size = 'large-v3'
+device = 'cuda'
+compute_type = 'float16'
+prompt = '使用繁體中文'
